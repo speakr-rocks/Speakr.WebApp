0916dfb82b38f16e7102603f0981379962440c4b
jplebre <jplebre@gmail.com>
2016-07-19 00:01:13 +0100

    Added form and validation to the homepage

--- src/Speakr.WebApp.Site/Controllers/HomeController.cs
+++ src/Speakr.WebApp.Site/Controllers/HomeController.cs
@@ -6,9 +6,26 @@ namespace Speakr.WebApp.Controllers
     public class HomeController : Controller
     {
         [Route("")]
-        public IActionResult Index()
+        public IActionResult Index(string Error = null)
         {
+            if (Error == "InvalidTalkId")
+            {
+                ModelState.AddModelError("InvalidTalkId", "Please enter a valid talk Id");
+            }
             return View();
+        }
+
+        [HttpGet]
+        [Route("GetRatingForm")]
+        public IActionResult GetRatingForm(string talkId)
+        {
+            // If talkid returns 404 from client:
+            if (!talkId.Equals("1234"))
+            {
+                return RedirectToAction("Index", new { Error = "InvalidTalkId" });
+            }
+
+            return RedirectToAction("DisplayForm", "Ratings", new { talkId = talkId });
         }
     }
 }
---
+++ tests/Speakr.WebApp.Site.Tests/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using Speakr.WebApp.Controllers;
+using System.Linq;
+
+namespace Speakr.WebApp.Site.Tests
+{
+    public class HomeControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IndexShouldNotReturnNull()
+        {
+            var controller = new HomeController();
+            Assert.That(controller.Index(), Is.Not.Null);
+        }
+
+        [Test]
+        public void GetRatingFormForValidTalkRedirectsCorrectly()
+        {
+            var controller = new HomeController();
+            var actionResult = (RedirectToActionResult)controller.GetRatingForm("1234");
+
+            Assert.That(actionResult.ActionName, Is.EqualTo("DisplayForm"));
+            Assert.That(actionResult.ControllerName, Is.EqualTo("Ratings"));
+        }
+
+        [Test]
+        public void GetRatingFormForInvalidTalkRedirectsWithError()
+        {
+            var controller = new HomeController();
+            var actionResult = (RedirectToActionResult)controller.GetRatingForm("1111");
+            var expectedErrorMessage = "InvalidTalkId";
+
+            Assert.That(actionResult.ActionName, Is.EqualTo("Index"));
+            Assert.That(actionResult.ControllerName, Is.Null);
+            Assert.That(actionResult.RouteValues.Count, Is.EqualTo(1));
+            Assert.That(actionResult.RouteValues.Values.First, Is.EqualTo(expectedErrorMessage));
+        }
+    }
+}
