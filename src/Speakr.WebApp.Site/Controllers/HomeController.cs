24346751bc986af5c4b8e2bf1c713df8431f24ce
jplebre <jplebre@gmail.com>
2016-07-27 21:53:53 +0100

    Working modelview validation

--- src/Speakr.WebApp.Site/Controllers/HomeController.cs
+++ src/Speakr.WebApp.Site/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Speakr.WebApp.Site.ViewModels.Home;
 
 namespace Speakr.WebApp.Controllers
 {
     [Route("/")]
     public class HomeController : Controller
     {
+        [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
+            return View("Index");
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult CheckTalkIdCode(CheckTalkIdViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return Content("Talk id is:" + model.TalkId);
+            }
             return View("Index");
         }
     }
