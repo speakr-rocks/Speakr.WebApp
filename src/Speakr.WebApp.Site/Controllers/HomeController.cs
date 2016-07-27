d74eeca27427e9848a177af28a6b3fa54b11e081
jplebre <jplebre@gmail.com>
2016-07-27 20:01:42 +0100

    Clean solution ready to work

--- src/Speakr.WebApp.Site/Controllers/HomeController.cs
+++ src/Speakr.WebApp.Site/Controllers/HomeController.cs
@@ -2,30 +2,13 @@
 
 namespace Speakr.WebApp.Controllers
 {
-    [Route("/"), Route("Home")]
+    [Route("/")]
     public class HomeController : Controller
     {
         [Route("")]
-        public IActionResult Index(string Error = null)
+        public IActionResult Index()
         {
-            if (Error == "InvalidTalkId")
-            {
-                ModelState.AddModelError("InvalidTalkId", "Please enter a valid talk Id");
-            }
-            return View();
-        }
-
-        [HttpGet]
-        [Route("GetRatingForm")]
-        public IActionResult GetRatingForm(string talkId)
-        {
-            // If talkid returns 404 from client:
-            if (!talkId.Equals("1234"))
-            {
-                return RedirectToAction("Index", new { Error = "InvalidTalkId" });
-            }
-
-            return RedirectToAction("DisplayForm", "Ratings", new { talkId = talkId });
+            return View("Index");
         }
     }
 }
