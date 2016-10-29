3a248fa063480707f4703667e9d460bc1554aca0
jplebre <jplebre@gmail.com>
2016-10-29 23:23:17 +0100

    I think this never actually worked properly. need to merge and push to prod

--- Install-App.ps1
+++ Install-App.ps1
@@ -9,5 +9,5 @@
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
 # To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
-C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\AppPools\Default Web Site' -Name managedRuntimeVersion -Value ""}
+C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\AppPools\DefaultAppPool' -Name managedRuntimeVersion -Value ""}
 C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\Speakr.WebApp\publish}
