d8dc5228a6003a574fc21a99869f94d9b0ae7205
jplebre <jplebre@gmail.com>
2017-03-23 23:12:14 +0000

    Refactor startup classes

---
+++ src/Speakr.WebApp.Site/AppStart/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Speakr.WebApp.Site.AppStart
+{
+    public class Configuration
+    {
+        public static IConfigurationRoot Configure()
+        {
+            var builder = new ConfigurationBuilder();
+
+            return builder.Build();
+        }
+    }
+}
